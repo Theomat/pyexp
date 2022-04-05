6a42a2f4ca5dc7108166ee358cbcfdb1b310fbbf
Theomat <theomatricon@gmail.com>
2022-04-05 17:45:01 +0200

    Fixed autocompletion of add

--- autocomplete/_comp_pyexp.sh
+++ autocomplete/_comp_pyexp.sh
@@ -24,20 +24,18 @@ _comp_pyexp()
             COMPREPLY=($(compgen -W "-h, --help" -- "${COMP_WORDS[2]}"))
             return 0
         fi
-    elif [ "${#COMP_WORDS[@]}" == "4" ]; then
-        if [[ "$has_name_opt save" == *"${COMP_WORDS[1]}"* ]] && [[ "-n --name" == *"${COMP_WORDS[2]}"* ]]; then
-            list="$(ls ~/.pyexp/)"
-            list=("${list[@]/_selected}")
-            COMPREPLY=($(compgen -W "$list" -- "${COMP_WORDS[3]}"))
-            return 0
-        fi
+    elif [ "${#COMP_WORDS[@]}" == "4" ] && [[ "$has_name_opt save" == *"${COMP_WORDS[1]}"* ]] && [[ "-n --name" == *"${COMP_WORDS[2]}"* ]]; then
+        list="$(ls ~/.pyexp/)"
+        list=("${list[@]/_selected}")
+        COMPREPLY=($(compgen -W "$list" -- "${COMP_WORDS[3]}"))
+        return 0
     elif [ "${#COMP_WORDS[@]}" == "5" ] && [ "${COMP_WORDS[1]}" == "save" ] && [[ "-n --name" == *"${COMP_WORDS[2]}"* ]]; then
         COMPREPLY=($(compgen -f -- "${COMP_WORDS[4]}"))
         return 0
+    elif [ "${#COMP_WORDS[@]}" != "1" ] && [ "${COMP_WORDS[1]}" == "add" ]; then
+        COMPREPLY=($(compgen -f -d -- "${COMP_WORDS[-1]}"))
+        return 0
     fi
-    
-    
-
     return 0
 }
 complete -o filenames -o nospace -o bashdefault -F _comp_pyexp pyexp
